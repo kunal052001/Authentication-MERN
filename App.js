1d301261ff8dc2466ded2ac948a06aee1a41ab80
kunal052001 <kunalsadashivpatil@gmail.com>
2025-01-01 11:09:24 +0530

    reggister functionality added

--- App.js
+++ App.js
@@ -7,7 +7,7 @@ const authRoutes = require('./routes/authRoutes');
 const app = express();
 const PORT = 3000;
 
-// Connect to MongoDB
+
 connectDB();
 
 // Middleware
@@ -16,15 +16,15 @@ app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// Routes
+
 app.use('/auth', authRoutes);
 
-// Home Route
+
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
-// Start Server
+
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
